8d8399b45312f6b1e8124ed2a04c2e277f76fa13
manjini <manjini.abraham@gmail.com>
2023-07-10 14:14:54 -0400

    updated js for searchlisting

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,10 @@ var x;
 
 $(function() {
   $("#searchbtn").on("click", searchCity);
+  renderSearchHistory();
+  $("button[class*='search-btn']").on("click", function() {
+    var city = $(this).text();  
+  });
 });
 
 //search button funtionality . here i am taking the value from the text input and checking if its already in the array. if yes do not duplicate else add it to the array searchItems.
@@ -22,6 +26,33 @@ function searchCity(){
 
   renderSearchHistory();
 
-  getWeather(city);
- 
+  
 }
+//here i am using the array searchItems and displaying the list in the left section as buttons
+
+function renderSearchHistory() {
+
+    var div = $("#searchListContainer"); // The div container where the list will be appended
+   
+    var ul = $("<ul>");// Create the <ul> element
+  
+    // Loop through each item in the searchItems array
+    for (var i = 0; i < searchItems.length; i++) {
+      var item = searchItems[i];
+  
+      // Create a button element for the item
+      var button = $("<button>").text(item).addClass("search-btn");
+  
+      // Create an <li> element for the button
+      var li = $("<li>").append(button);    
+  
+      // Append the <li> element to the <ul> element
+        ul.append(li);
+    }
+  
+    if (searchItems.length > 0){
+      div.empty(); //clearing the div
+      div.append(ul); // Append the <ul> element to the div container
+    }
+   
+  }
